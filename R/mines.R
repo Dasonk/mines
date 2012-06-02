6e66d10b866ea80e3589da665cde972bff145944
Dason Kurkiewicz <dasonk@iastate.edu>
2012-06-02 17:34:42 -0500

    Put functions into own files

---
+++ R/mines.R
@@ -0,0 +1,32 @@
+
+#' A quick way to play Mines.
+#' 
+#' This is an easier way to plays Mines.  Instead of using mines.custom to create the size
+#' of the board you want and to specify how many mines.  This allows you to use a few presets.
+#' 
+#' @param size The size of the minefield.  Can be either "small" "medium" or "large".
+#' Only the first letter matters.
+#' @return Returns nothing but creates a game.
+#' @import mines.custom.R
+#' @author Dason Kurkiewicz \email{dasonk@@iastate.edu}
+#' @export
+mines <- function(size = "small"){
+    size <- toupper(substr(size, 1, 1))
+    if(size == "S"){
+        n <- 9
+        m <- 9
+        n.mines <- 10
+    }else if(size == "M"){
+        n <- 16
+        m <- 16
+        n.mines <- 40
+    }else if(size == "L"){
+        n <- 16
+        m <- 30
+        n.mines <- 99
+    }else{
+        gmessage("Please only choose small, medium, or large for size")
+        return()
+    }
+    mines.custom(n, m, n.mines)
+}
