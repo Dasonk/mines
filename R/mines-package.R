5cc6b2bec1848c8789a3eed4b008cb65f352d279
Dason Kurkiewicz <dasonk@iastate.edu>
2012-09-21 23:44:34 -0500

    Changed gWidgetsRGtk2 to imports

---
+++ R/mines-package.R
@@ -0,0 +1,9 @@
+#' A Minesweeper clone for R
+#'
+#' Want to play Mines?  Hell yes you do.
+#'
+#' @docType package
+#' @name mines-package
+#' @import gWidgetsRGtk2
+#' @aliases mines-package package-mines
+NULL
